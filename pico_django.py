eb029d6b0446ae7c8d4e972c9f99a32a3263be60
Tim Watts <tim@readevalprint.com>
2014-04-17 16:09:42 +0200

    Update for django 1.6

--- pico_django.py
+++ pico_django.py
@@ -1,16 +1,21 @@
 from django.http import HttpResponse
-from django.conf.urls.defaults import patterns
+from django.conf.urls import url
+
 DEBUG=True
 ROOT_URLCONF = 'pico_django'
 DATABASES = { 'default': {} }
+
 def index(request, name):
     return HttpResponse('Hello {name}!'.format(name=(name or 'World')))
 
-urlpatterns = patterns('', (r'^(?P<name>\w+)?$', index))
+urlpatterns = [
+        url(r'^(?P<name>\w+)?$', index)
+        ]
+
+SECRET_KEY="notsosecret"
 
 # run with djagno dev server
 # $ PYTHONPATH=. django-admin.py runserver 0.0.0.0:8000 --settings=pico_django
 
 # for example run with uwsgi
 # $ uwsgi -s 127.0.0.1:3031 -M --pythonpath=/path/to/this/dir --env DJANGO_SETTINGS_MODULE=pico_django -w "django.core.handlers.wsgi:WSGIHandler()"
-
