4b3e27df5552df71584291c722647ba6d31d4f12
Tim Watts <readevalprint@users.noreply.github.com>
2015-07-13 16:20:18 +0200

    update pico_django for Django 1.8

--- pico_django.py
+++ pico_django.py
@@ -1,21 +1,22 @@
 from django.http import HttpResponse
 from django.conf.urls import url
 
-DEBUG=True
+DEBUG = True
 ROOT_URLCONF = 'pico_django'
-DATABASES = { 'default': {} }
+DATABASES = {'default': {}}
+
 
 def index(request, name):
     return HttpResponse('Hello {name}!'.format(name=(name or 'World')))
 
-urlpatterns = [
-        url(r'^(?P<name>\w+)?$', index)
-        ]
+urlpatterns = (
+    url(r'^(?P<name>\w+)?$', index)
+)
 
-SECRET_KEY="notsosecret"
+SECRET_KEY = "not so secret"
 
 # run with djagno dev server
 # $ PYTHONPATH=. django-admin.py runserver 0.0.0.0:8000 --settings=pico_django
 
 # for example run with uwsgi
-# $ uwsgi -s 127.0.0.1:3031 -M --pythonpath=/path/to/this/dir --env DJANGO_SETTINGS_MODULE=pico_django -w "django.core.handlers.wsgi:WSGIHandler()"
+# `$ uwsgi --http :8000 -M --pythonpath=. --env DJANGO_SETTINGS_MODULE=pico_django -w "django.core.handlers.wsgi:WSGIHandler()"`
